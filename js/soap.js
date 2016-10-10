ba9258a1337735f2cff6074fa800a7d7a91b094d
Pavel Galaton <pavel.galaton@gmail.com>
2016-10-10 21:09:02 +0300

    another small refactoring

--- js/soap.js
+++ js/soap.js
@@ -10,7 +10,7 @@ let SOAP = {
         });
     },
     _buildEnvelope: function (token, xmlns, body) {
-            return `<?xml version="1.0" encoding="UTF-8"?>
+        return `<?xml version="1.0" encoding="UTF-8"?>
 <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope" xmlns="` + xmlns + `">
     <soap:Header>
         <context>
@@ -23,17 +23,26 @@ let SOAP = {
 </soap:Envelope>`;
     },
     authRequest: function (account, password, callback, errorCallback) {
-        $.get('soap/authRequest.xml')
-            .done(function(xml){
-                $.ajax({
-                    type: "POST",
-                    url: this._url,
-                    data: xml.replace(/ACCOUNT_PLACEHOLDER/g, account)
-                            .replace(/PASSWORD_PLACEHOLDER/g, password)
-                })
-                .done(callback)
-                .fail(errorCallback || function () {});
-            });
+        return $.ajax({
+            type: "POST",
+            url: this._url,
+            data: `<?xml version="1.0" encoding="UTF-8"?>
+<soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope">
+    <soap:Header>
+        <context xmlns="urn:zimbraAccount">
+        </context>
+    </soap:Header>
+    <soap:Body>
+        <AuthRequest xmlns="urn:zimbraAccount">
+            <account by="name">` + account + `</account>
+            <password>` + password + `</password>
+        </AuthRequest>
+    </soap:Body>
+</soap:Envelope>
+`
+            })
+            .done(callback)
+            .fail(errorCallback || function () {});
     },
     getFolderRequest: function (token, callback) {
         return this._send(
@@ -43,14 +52,13 @@ let SOAP = {
     },
     search: function (token, query, callback) {
         return this._send(
-            this._buildEnvelope(token, 'urn:zimbraMail', `<SearchRequest><query>` + query +`</query></SearchRequest>`)
+            this._buildEnvelope(token, 'urn:zimbraMail', '<SearchRequest><query>' + query +'</query></SearchRequest>')
         )
         .done(callback);
     },
     getAppointments: function (token) {
         return this._send(
-            this._buildEnvelope(token, 'urn:zimbraMail', '<SearchRequest types="appointment" calExpandInstStart="` + getUTCNow() + `" sortBy="dateDesc">'+
-            '<query>is:anywhere</query></SearchRequest>')
+            this._buildEnvelope(token, 'urn:zimbraMail', '<SearchRequest types="appointment" calExpandInstStart="' + getUTCNow() + '" sortBy="dateDesc"><query>is:anywhere</query></SearchRequest>')
         )
     },
     getAppointment: function (token, uid) {
